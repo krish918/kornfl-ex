ce36da07b0dcef077d27ca400177794b2cf453dd
Krishna Murti <s3_krish@yahoo.co.in>
2012-11-04 03:41:27 +0530

    404 Error page

---
+++ src/error/notfound.php
@@ -0,0 +1,55 @@
+<!doctype HTML>
+
+ <html>
+    <head>
+	
+	  <title> 404 : Page not found </title>
+	   <meta name="description" content='Kornflex is a web utility by AHENS, which enhances and helps your
+				campus life, in easy and exclusive ways.' />   
+		<meta charset="utf-8" />			
+		<link rel="shortcut icon" href="../favicon.ico"/> 
+		
+		<link rel="stylesheet" type="text/css" href="http://kf.ahens.com/kornflex_design_all/kf_design_1366.css" />
+		<link rel="stylesheet" type="text/css" href="http://kf.ahens.com/kornflex_design_all/kf_design_error_1366.css" />
+			<script type="text/javascript" src="http://kf.ahens.com/script/kf_script.js"> </script>
+			 <script type="text/javascript">
+	 var width=(window.innerWidth)?window.innerWidth:document.documentElement.offsetWidth;
+	 if(width<1290)
+	  {
+	    var head = document.getElementsByTagName('head')[0];
+		var link = document.createElement("link");
+		link.type="text/css";
+		link.rel="stylesheet";
+		link.href="http://kf.ahens.com/kornflex_design_all/kf_design_1024.css";
+		var link2 = document.createElement("link");
+		link2.type="text/css";
+		link2.rel="stylesheet";
+		link2.href="http://kf.ahens.com/kornflex_design_all/kf_design_error_1024.css";
+		head.appendChild(link);
+		head.appendChild(link2);
+	
+		
+	  }
+	</script>  
+	</head>
+    <body class="notFound" id="error_404">
+	   <div class="a404_panel">
+           <div class="ahns_cntnr" id="ahens_copyright">
+		       AHENS &copy; 2012 | All rights reserved
+			   <br />
+			  <span style="font-style:italic;font-size:12px;">  Application version 2.0.0.0</span>
+		   </div>
+		   <div class="a_404_dialog" id="404_main_dialog">
+		        This is an abondoned plot!
+		   </div>
+		   <div class="subTitle"> You are at 404, ahens. Nobody lives here.  </div>
+		   <div class="text_content" id="elaborated">
+		        It seems you have lost your way. This ocurrs while trying to reach a place, which actually
+				doesn't exist. You requested for a page which is not present on AHENS. Please check the name of the
+				page. It might be possible that you are right, but AHENS has moved or deleted that page.<br /><br />
+				 
+		   </div>
+		 </div>  
+	</body>	   
+</html>	
+			
