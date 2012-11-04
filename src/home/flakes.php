54d7f98c93a9292d64cf914588847bd6b91e40df
Krishna Murti <s3_krish@yahoo.co.in>
2012-11-04 13:03:43 +0530

    For visiting people's flakesbook

---
+++ src/home/flakes.php
@@ -0,0 +1,332 @@
+<?php
+/*
+ Copyright 2012 KRISHNA MURTI
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+      $con = mysql_connect("127.0.0.1","<db-username>","<db-password>");
+	  if(!$con)
+	     {
+		header("location:../disconnected.html");
+		exit;
+	     }
+      mysql_select_db("<db-name>",$con);
+      session_start();
+      $username = '';
+      $query = mysql_query("SELECT * FROM corn_users");
+	  $indiantime = time()+45000;
+	  $dat =date("F:d, Y | h:i a",$indiantime);
+	  
+      while($row = mysql_fetch_array($query))
+       {
+          if(($row['Email']==$_SESSION['user'])||($row['Email']==$_COOKIE['user']))
+             {
+               $uid = $row['userID'];
+               $username = $row['userName'];
+	       if($row['campusJoined'] == '')
+                       {header("location:http://kf.ahens.com/error/noaccess/?access=denied"); exit;}	
+		else $campCode =$row['campusJoined'];
+             }
+       }
+	   mysql_query("INSERT INTO site_stat (userID, page_address,timestamp) VALUES ('$uid','$_SERVER[REQUEST_URI]','$dat')",$con); 
+	  if(isset($_GET['signout']))
+	  {
+	   mysql_query("UPDATE corn_users SET lastIP = '$_SERVER[REMOTE_ADDR]', lastUserInfo = '$_SERVER[HTTP_USER_AGENT]' WHERE Email ='$_SESSION[user]'",$con);
+			$current = time();
+			$spent = $current - $_SESSION['time'];
+			mysql_query("UPDATE corn_users SET timeSpent = timeSpent + '$spent'  WHERE Email ='$_SESSION[user]'",$con);
+			mysql_query("UPDATE corn_users SET live = 0 WHERE Email = '$_SESSION[user]'", $con);
+	    	 setcookie("user","",time()-3600,'/');
+			session_destroy();
+			header("location:http://kf.ahens.com/?logout=success");
+			exit;
+	  }
+		if(($_SESSION['user']=='')&&($_COOKIE['user']==''))	
+		{  $_SESSION['url_temp']=$_SERVER['REQUEST_URI'];
+		header("location:http://kf.ahens.com/login/?loginrequired=1"); exit;} 
+	$_SESSION['url_temp']='';	 
+	  $noflakes =0;
+	  $noabt =0;
+	  $insert=0;
+        
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD html 4.01 transitional //EN" "http://www.w3.org/TR/html4/loose.dtd">
+<HTML class="kornflakesFlakes" id="FlakesShowKornflakes" lang="en">
+  <HEAD>
+      <TITLE> <?php echo $username;?>'s Flakes</TITLE>
+       <META name="content" description="kornflakes is aiming to create a huge campus network. Log in to your campus, create the best profile you can,  project your profile and speak whatever you can" />
+       <LINK type="shortcut icon" href="../images/korn_ico.ico" />
+       <LINK type="text/css" rel="stylesheet" href="http://kf.ahens.com/korn_style.css" />
+       <SCRIPT type="text/javascript" src="http://kf.ahens.com/korn_script.js"></SCRIPT>
+  </HEAD>
+  <BODY class="kornContent" onload="loadAllFlakes(),startNotification(), updatenotif(),loginstatus()">
+   <SCRIPT type="text/javascript">
+             function loadAllFlakes()
+              {
+                 document.getElementById("msg").innerHTML="<IMG src='/images/ajax-loader.gif' alt='loading' border=0 />";
+                 var httpc;
+                  if(window.XMLHttpRequest)
+                     httpc = new XMLHttpRequest();
+                  else
+                     httpc = new ActiveXObject("Msxml2.XMLHTTP");
+                   httpc.onreadystatechange= function()
+                      {
+                        if(httpc.readyState==4 && httpc.status==200)
+                          {     
+                               document.getElementById("msg").innerHTML="";
+                              document.getElementById("flakesBody").innerHTML = httpc.responseText;
+                          }
+                       }
+                    httpc.open("get","flakesLoader.php?getal=1", true);
+                    httpc.send();
+                 }
+                   
+        function loadFlakes()
+         {
+            var flakes = document.getElementById("flakes").value;
+            var about = document.getElementById("about").value; 
+			flakes = escape(flakes);
+			about = escape(about);
+            if((document.getElementById("flakes").value=="")||(document.getElementById("flakes").value=="Start here, say something."))
+                   document.getElementById("msg").innerHTML = "<DIV Style='background-color:#ffdddd;border:solid 1px #ff9090;width:500px;'>Your Flakes is missing!</div>";
+            else if((document.getElementById("about").value=="")||(document.getElementById("about").value=="I am saying something about"))
+                   document.getElementById("msg").innerHTML = "<DIV Style='background-color:#ffdddd;border:solid 1px #ff9090;width:500px;'>Please write about what your Flakes exactly is!</div>";
+             else
+                {
+                  document.getElementById("msg").innerHTML="<IMG src='http://kf.ahens.com/images/ajax-loader.gif' alt='loading' border=0 />";
+                  var httpf;
+                  if(window.XMLHttpRequest)
+                     httpf = new XMLHttpRequest();
+                  else
+                     httpf = new ActiveXObject("Msxml2.XMLHTTP");
+                   httpf.onreadystatechange= function()
+                      {
+                        if(httpf.readyState==4 && httpf.status==200)
+                          {     
+                              document.getElementById("msg").innerHTML="";
+                             
+                              document.getElementById("flakesBody").innerHTML = httpf.responseText;
+                          }
+                       }
+                    httpf.open("get","flakesLoader.php?flakes="+flakes+"&about="+about, true)
+                    httpf.send();
+                 }
+            }    
+   </SCRIPT>
+    <DIV class="mainContainer" style="font-family:tahoma;font-size:13px;text-align:left;width:500px;padding-left:194px;padding-right:196px;">
+    <br />   <br />
+    <DIV style="background-color:#eeffee;text-align:right;border:solid 1px #ccffcc;font-family:arial;font-style:italic;font-size:29px;">
+    FLAKES&trade;
+    </DIV>
+    <br />
+    <DIV style="color:#909090;font-size:12px;text-align:right;font-style:italic;font-family:verdana;">Flakes is a plural word, as it sounds and will always remain plural.<br />
+    Flakes is anything you speak about people, things, ideas & feelings around you and share it in your network.
+    </DIV>
+    <br /><br /><br /><br /><br /><br /><br /><br /><br />
+    <DIV style="font-family:georgia,verdana,tahoma;font-size:32px;color:purple;">Share Seamlessly</DIV>
+    <br /><DIV style="font-family:verdana;font-size:18px;">It's simple & fun!</div><br />
+  
+    <TEXTAREA id="flakes" name="flakes" style="width:500px;height:60px;font-family:tahoma;border:solid 1px #ccddee;color:#aaaaaa;overflow:auto;" onblur="bringw()" onfocus="areaBig()">Start here, say something.</TEXTAREA>
+    <br /><br />
+    <INPUT id="about" name="abt" onfocus="textclear()" onblur="bring()" type="text" style="border:solid 1px #ccddee;height:30px;color:#aaaaaa;width:500px;" value="I am saying something about" />
+  
+    <br /><br />
+    <button type="button" id="button" name="flaker" style="width:500px;" onmouseover="this.style.backgroundColor='#ccccdd';" onmouseout="this.style.backgroundColor='#dddddd';" onmousedown="this.style.borderStyle='ridge';" onmouseup="this.style.borderStyle='groove'; this.style.backgroundColor='#dddddd';" onclick="loadFlakes()">Start Flaking</Button>
+    
+    <br /><br />
+    <?php
+       if($_GET['deleted']!='')
+         {
+         if($_GET['deleted']==1)
+              echo "<DIV style='background-color:#ffdddd;border:solid 1px #ff9090;width:500px;'>Your Flakes was successfully deleted!</DIV>";
+           else if($_GET['deleted']==0)
+             echo "<DIV style='background-color:#ffdddd;border:solid 1px #ff9090;width:500px;'>Some Error ocurred at ahens server. Please try later!</DIV>"; 
+          }  
+        if($_GET['hidden']!='')
+          {   
+          if($_GET['hidden']==1)
+              echo "<DIV style='background-color:#ffdddd;border:solid 1px #ff9090;width:500px;'>Your Flakes was hidden. It will not be available in common room now.</DIV>";
+           else if($_GET['hidden']==0)
+             echo "<DIV style='background-color:#ffdddd;border:solid 1px #ff9090;width:500px;'>Some Error ocurred at ahens server. Please try later!</DIV>"; 
+          }
+        if($_GET['show']!='')
+          {     
+          if($_GET['show']==1)
+              echo "<DIV style='background-color:#ffdddd;border:solid 1px #ff9090;width:500px;'>Your Flakes is now available in common room.</DIV>";
+           else if($_GET['show']==0)
+             echo "<DIV style='background-color:#ffdddd;border:solid 1px #ff9090;width:500px;'>Some Error ocurred at ahens server. Please try later!</DIV>"; 
+          }  
+          
+    
+    ?>                  
+     <DIV id="msg" style="text-align:center;">
+     </DIV><br />
+     <DIV id="flakesBody"></DIV> 
+    <DIV style="font-family:'lucida sans unicode'; color:#ff90bb;text-align:center;">FLAKES&trade; is appreciably abuse-sensitive. Dive in fearlessly! </DIV><br /><br /><hr />
+		<DIV id="end" style="position:absolute;bottom:-90px;left:250px;height:40px;">
+	     <br />AHENS &copy; 2012
+	  </DIV> 
+	</DIV>
+  <div class="bottombar" style="left:0px;width:1347px;"> 
+     <div style="position:absolute; left: 30px;top: 2px;">
+           <img src="http://kf.ahens.com/images/ahensfinalsmall.png" />
+     </div>		   
+    <div style="position:absolute; left:600px; top:10px;color:#656565;">
+     ahens &copy; 2012
+	 </div>
+     <div style="position:absolute; left:1060px; top:10px;" id="link" onclick="location.href='http://kf.ahens.com/home/ahens/about.php';"> About </div>
+	 <div style="position:absolute; left:1160px; top:10px;" id="link" onclick="location.href='http://kf.ahens.com/home/ahens/help.php';"> Help </div>
+	 <div style="position:absolute; left:1260px; top:10px;" id="link" onclick="location.href='http://kf.ahens.com/home/ahens/privacy.php';"> Privacy </div>
+  </div>
+   
+     <!--     <DIV class="upLogin" style="font-size:14px;">
+				<span class="upperLink" style="left:10px;" onclick="location.href='search/';">Search Network</span>
+				<span class="upperLink" style="left:160px;" onclick="location.href='../home/';"> My Profile </span>
+<span style="position:absolute;top:5px;left:305px;"><INPUT type="text" size="40" style="color:#989898;border:groove 1px #ff8080;text-align:center;font-family:'lucida sans unicode';" class="simple" id="qs" value="Quick Search" onfocus="clears(this.value)" onblur="get(this.value)" onkeyup="quicksearch(this.value,'<?php echo $campCode; ?>')"/></span>						
+	          <DIV id="menuUser" onmouseover="menuOver()" onmouseout="menuOut()" onclick="showHideMenu()">
+                  <?php echo $username;?>
+                  
+                  </DIV>
+            </DIV>  -->
+
+	
+	<DIV class="midPlate" style="top:265px;">
+	  <span style="position:absolute;left:430px;font-family:tahoma;font-size: 26px;font-style:bold;">
+	  <?php 
+	        $brow = mysql_query("SELECT * FROM campusdata");
+			while($r = mysql_fetch_array($brow))
+			  {
+				if($r['campusCode']==$campCode)
+				{
+				  
+			      echo $r['campusName'];
+			       ?> </span><br /><span style="position:absolute;left:430px;top: 40px;color:#cccccc;">
+				   <?php
+			        if($r['Address1']!='')
+			           echo $r['Address1'].","."<br />";
+			       if($r['Address2']!='')
+			             echo $r['Address2'].","."<br />";
+			         if($r['City']!='')
+			             echo $r['City'].","."<br />";
+			           if($r['State']!='')
+			                echo $r['State'].","."<br />";
+			         
+			           if($r['Country']!='')
+			             echo $r['Country']; 
+				   ?></span><span style="position:absolute;left:915px;font-size:14px;font-family:verdana;top:34px;border-style:solid;border-width:0px 0px 0px 1px;padding-left:6px; color:#bbbbbb;"><?php
+				   echo $r['Established']." Established<br />";
+				   ?><span style="font-size:26px;">
+				   <?php
+				   echo $r['networkCount']. "</span> People in network<br />";
+				   ?><span style="font-size:26px;"><?php
+				   echo $r['projectionCount']."</span> Projected profiles";
+				   ?></span><?php
+				}
+			  }
+	    ?>
+	
+    </DIV>
+		    <DIV id="searchbox" style="text-align:center;"></DIV>
+			  <div class="deck">
+		<div class="decklink" onclick="location.href='http://kf.ahens.com/home/search';">Search</div>
+		<div class="decklink" style="left:270px;" onclick="location.href='http://kf.ahens.com/home/profile.php';">Profile</div>
+		<div style="position:absolute;top:5px;left:490px;">
+		  <input type="text" value="Enter a name to search" id="qs" class="qsearchtextbox" onfocus="clears(this.value)" onblur="get(this.value)" onkeyup="quicksearch(this.value,'<?php echo $campCode; ?>')"/>
+		 </div> 
+		 <div id="backProEff" onclick="document.getElementById('menunew').style.visibility='hidden';this.style.visibility='hidden';"> </div>
+		 <div id="callmenu" style="position:absolute;top:1px;left:950px;cursor:pointer;"><img src="http://kf.ahens.com/images/profile.png" border=0 /></div> 
+				   
+				   <script>
+					  var cal = document.getElementById("callmenu");
+					  var shown=0;
+					  cal.onclick = function()
+								{
+								wtimer(); 
+								if(shown==0)
+								 {
+									document.getElementById('menunew').style.visibility='visible';
+									document.getElementById('backProEff').style.visibility='visible';
+									shown =1;
+								  }
+								 else
+									{
+									  document.getElementById('menunew').style.visibility='hidden';
+									  document.getElementById('backProEff').style.visibility='hidden';
+									  shown=0;
+									}  
+								};
+								
+					</script>			
+		    <div class="menunew" id="menunew">
+			  
+				   <div style="position:absolute; top:-21px; right:40px;width:0;height:0; border-right:20px solid transparent;border-left:20px solid transparent;border-bottom:20px solid #aaaaaa;">
+			       </div>	
+				   <div style="position:absolute; top:-20px; right:40px;width:0;height:0; border-right:20px solid transparent;border-left:20px solid transparent;border-bottom:20px solid rgb(247,247,247);">
+			       </div>
+                   <div style="padding:8px;border-bottom:1px solid #aaaaaa;font-weight:bold;font-size:15px;"><?php echo $username; ?> </div>
+					<div style="padding:8px;" id="menulnk" onclick="enlarge('timer',11,0)"> Whistle <div style="position:absolute;right:5px;font-style:italic;font-family:arial;font-size:11px;display:inline-block;;color:#777777;" id="timer"></div></div> 
+					<div style="padding:8px;" id="menulnk" onclick="location.href='http://kf.ahens.com/home/ahens/theahensbuilders.php';">Credits</div>
+					<div style="border-bottom:1px solid #cccccc;"></div> 
+					<div style="padding:8px;" id="menulnk" onclick="location.href='http://kf.ahens.com/home/ahens/help.php';">Help</div>
+					<div style="padding:8px;" id="menulnk" onclick="location.href='http://kf.ahens.com/home/ahens/settings.php';">Settings</div>
+					<div style="border-bottom:1px solid #cccccc;"></div> <form action="" method="get">
+                     <button style="padding:8px;border-style:none;background-color:transparent;width:190px;text-align:left;font-family:arial;" id="menulnk" type="submit" name="signout" >Log out</button></form>
+					<div style="padding:0px 5px 5px 5px;font-size:9px;color:#606060;font-family:tahoma;">AHENS TRIVIA : The name 'ahens' was generated accidently, through a very simple program written in C !
+						</div>
+			</div> 
+  </div>
+<!--	<DIV class="menu" id="menu" onmouseover="menuOver()" onmouseout="menuOut()">
+	 <FORM action="" method="GET">
+	   <DIV class="menuin" style="position:absolute;top:10px;cursor:pointer;" onmouseover="this.style.backgroundColor='#dddddd';" onmousedown="this.style.backgroundColor='#aa5656';" onmouseup="this.style.backgroundColor='#dddddd';" onmouseout="this.style.backgroundColor='#eeeeee';" onclick="location.href='ahens/settings.php';">Settings</DIV>
+	   <DIV class="menuin" style="position:absolute;top:45px;cursor:pointer;" onmouseover="this.style.backgroundColor='#dddddd';" onmousedown="this.style.backgroundColor='#aa5656';" onmouseup="this.style.backgroundColor='#dddddd';" onmouseout="this.style.backgroundColor='#eeeeee';" onclick="location.href='ahens/theahensbuilders.php';">The Ahens Builders</DIV>
+	   <DIV class="menuin" style="position:absolute;top:80px;cursor:pointer;" onmouseover="this.style.backgroundColor='#dddddd';" onmousedown="this.style.backgroundColor='#aa5656';" onmouseup="this.style.backgroundColor='#dddddd';" onmouseout="this.style.backgroundColor='#eeeeee';" onclick="location.href='ahens/help.php';">Help</DIV>
+	    <button name="signout" type="submit" class="menuin" style="position:absolute;top:115px;cursor:pointer;" onmouseover="this.style.backgroundColor='#dddddd';" onmousedown="this.style.backgroundColor='#aa5656';" onmouseup="this.style.backgroundColor='#dddddd';" onmouseout="this.style.backgroundColor='#eeeeee';">Log Out
+	    </button>
+		</FORM>
+	 </DIV> -->
+  <DIV class="glassSignup2">
+	<DIV class="sideLink2" id="sideLnk1" onmouseover="sidelinkover('sideLnk1')" onmouseout="sidelinkout('sideLnk1','Common Room')" onclick="location.href='users/commonroom.php';">
+	Common Room
+	</DIV>
+    <DIV class="sideLink2" id="sideLnk2">
+	<Form action="index.php" method ="get"><button id="ed" type="submit" name="editPro" style="font-size:15px;font-family:'tahoma';cursor:pointer;border-style:none;background-color:white;color:rgb(120,120,120);" onmouseover="sidelinkover('ed')" onmouseout="sidelinkout('ed', 'Edit Profile')">Edit Profile</button></form>
+	</DIV>
+    <DIV class="sideLink2" id="sideLnk3" onmouseover="sidelinkover('sideLnk3')" onmouseout="sidelinkout('sideLnk3','Mirror')" onclick="location.href='mymirror.php';">
+	Mirror
+	</DIV>
+	<DIV class="sideLink2" id="sideLnk4" onclick="location.href='flakes.php';">
+	Flakes&trade; &gt;
+	</DIV> 
+	<DIV class="sideLink2" id="sideLnk5"  onmouseover="sidelinkover('sideLnk5')" onmouseout="sidelinkout('sideLnk5','Stone&trade;')" onclick="location.href='http://kf.ahens.com/home/stone';">
+	Stone&trade;
+	</DIV>
+<DIV style="font-weight:bold;" class="sideLink2" id="sideLnk6"  onmouseover="sidelinkover('sideLnk6')" onmouseout="sidelinkout('sideLnk6','The New Home')" onclick="location.href='http://kf.ahens.com/home/home.php';">
+	 The New Home 
+	</DIV><br />
+	 <span style="font-size:15px;"> New ways to share your ideas. Soon with AHENS!</span>
+
+  
+ 
+    </DIV> 
+ 
+
+	
+    </DIV>
+	<DIV class="cover" id="cvr"></DIV>
+	  <DIV id="notif"></DIV>
+	  	 <DIV class="allnotification" id="shownotif" onclick="showallnotif(0,15)"> <li style="font-family:tahoma;font-size:14px;text-align:center;color:black;"> All Notifications</li></DIV>
+	 <DIV class="allnotification" style="visibility:hidden;" id="hidenotif" onclick="hidenotif()"> <li style="font-family:tahoma;font-size:14px;text-align:center;color:black;"> Hide All</li></DIV>
+	 <div id="notiFrame"><Div id="content"></DIV> <div id="more" style="text-align:center;"></div></div>
+   </BODY>
+   </HTML>
+	 
